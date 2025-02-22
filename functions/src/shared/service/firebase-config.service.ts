35905b7895166a8f49e4c43f9de0b7ff1b1802a3
esteban guzman <esteban.guzman@xid.xyz>
2025-02-22 02:03:18 -0500

    chore(ci): tesint v2 for detection environment

--- functions/src/shared/service/firebase-config.service.ts
+++ functions/src/shared/service/firebase-config.service.ts
@@ -4,7 +4,7 @@ import { injectable } from "inversify";
 @injectable()
 export class FirebaseConfigService {
   public getCredentials(): string {
-    const credentialsPath = process.env.FIREBASE_CREDENTIAL_JSON || "";
+    const credentialsPath = process.env.FIREBASE_CREDENTIALS_JSON || "";
 
     if (!fs.existsSync(credentialsPath)) {
       throw new Error(`Credentials file does not exist at path: ${credentialsPath}`);
