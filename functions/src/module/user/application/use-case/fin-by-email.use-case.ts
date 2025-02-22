666db7912495bf7c89685bfd433c645b108a36a4
esteban guzman <esteban.guzman@xid.xyz>
2025-02-21 22:38:49 -0500

    feat(user): add module for user

---
+++ functions/src/module/user/application/use-case/fin-by-email.use-case.ts
@@ -0,0 +1,12 @@
+import { inject, injectable } from "inversify";
+import { UserRecord } from "firebase-admin/auth";
+import { AuthRepository } from "@module/auth/domain/repositories/auth.repository";
+
+@injectable()
+export class FindByEmailUserUseCase {
+  constructor(@inject(AuthRepository) private authRepository: AuthRepository) {}
+
+  async execute(email: string): Promise<UserRecord | undefined> {
+    return await this.authRepository.findByEmail(email);
+  }
+}
