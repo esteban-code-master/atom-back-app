e4addece06185ff3d412ae129907ca9bbcac998b
esteban guzman <esteban.guzman@xid.xyz>
2025-02-22 01:32:37 -0500

    chore(test): testing acces environment from github action

--- functions/src/config/inversify.config.ts
+++ functions/src/config/inversify.config.ts
@@ -3,11 +3,13 @@ import { taskModule } from "@module/task/task.module";
 import { userModule } from "@module/user/user.module";
 import { FirestoreService } from "@shared/interface/firestore.service";
 import { FirebaseAdminService } from "@shared/service/firebase-admin.service";
+import { FirebaseConfigService } from "@shared/service/firebase-config.service";
 import { FirestoreServiceImpl } from "@shared/service/firestore.service";
 import { Container } from "inversify";
 
 const container = new Container();
-container.bind<FirebaseAdminService>(FirebaseAdminService).toSelf().inSingletonScope();
+container.bind(FirebaseConfigService).toSelf();
+container.bind(FirebaseAdminService).toSelf().inSingletonScope();
 container.bind<FirestoreService>(FirestoreService).to(FirestoreServiceImpl).inSingletonScope();
 
 container.load(authModule);
